fb1a791cec47d6957da487f99676d3e2f4077a99
YUNUS KARAGÜN <yunus.karagun@gmail.com>
2024-07-18 14:39:40 +0300

    Update Bulut_Tahsilat_POS_V3.py

--- Bulut_Tahsilat_POS_V3.py
+++ Bulut_Tahsilat_POS_V3.py
@@ -1,14 +1,19 @@
 import numpy as np
 import pandas as pd
+
 Bulut_All=pd.read_excel(r'D:\BulutTahsilat\2024-05\2024-05-BT.xlsx')
 Banka_Conf=pd.read_excel(r"D:\BulutTahsilat\Config.xlsx", sheet_name="Banka")
 Bulut_All["Tarih"]=Bulut_All["Tarih"].dt.date
+
 V3_Sablon=pd.read_excel(r"D:\BulutTahsilat\Config.xlsx", sheet_name="V3_Sablon")
+
 V3_Sablon_2=V3_Sablon[V3_Sablon["Belge No"]==2]
+
 Bulut_POS = Bulut_All[(Bulut_All["İşlem Tipi"] == "POS") & 
 ~((Bulut_All["Banka"] == "HALKBANK") & Bulut_All["Açıklama"].str.contains("POS Aidat"))&
 ~(Bulut_All["Banka"] == "GARANTİ BBVA")&
 ~(Bulut_All["Banka"] == "İŞ BANKASI")].copy()
+
 def determine_kod(row):
     if row['Banka'] == 'ZİRAAT BANKASI' and row['Hareket Tipi'] == 'ALACAK':
         return 'ZB-P'
@@ -58,24 +63,33 @@ def determine_kod(row):
 Bulut_POS['KOD'] = Bulut_POS.apply(determine_kod, axis=1)
 
 Bulut_POS['KOD1']= Bulut_POS['KOD'].str.split('-').str[1]
+
+# Bulut_POS_Kodlu=Bulut_POS[["Hareket Tipi", "İşlem Kodu", "Banka", "Tutar", "Tarih", "Firma IBAN", "KOD", "KOD1"]].reset_index(drop=True)
+
 POS_Toplam=Bulut_POS.groupby(['Tarih', 'Banka', "Firma IBAN", 'KOD',"KOD1"])['Tutar'].sum().reset_index()
 
+
 POS_Ozet = POS_Toplam.pivot_table(index=['Banka', "Firma IBAN", 'Tarih'], columns='KOD1', values='Tutar', fill_value=0).reset_index()
+
 
 POS_Ozet["Banka1"]= POS_Ozet["P"]+POS_Ozet["PI"]
 POS_Ozet["Pos"]=-POS_Ozet["Banka1"]
 POS_Ozet["Banka2"] =POS_Ozet["K"]+POS_Ozet["KI"]
 POS_Ozet["Komisyon"]=-POS_Ozet["Banka2"]
 
-melted_df = pd.melt(POS_Ozet, id_vars=["Banka", "Firma IBAN", "Tarih"], value_vars=["Banka1", "Pos", "Banka2", "Komisyon"], 
+POS_Ozet.reset_index(inplace=True)
+
+melted_df = pd.melt(POS_Ozet, id_vars=["index", "Banka", "Firma IBAN", "Tarih"], value_vars=["Banka1", "Pos", "Banka2", "Komisyon"], 
                     var_name="Type", value_name="Tutar")
 mapping = {'Banka1': 1, 'Pos': 2, 'Komisyon': 3, 'Banka2': 4}
 
 melted_df["Sort"]= melted_df['Type'].map(mapping)
-melted_df.sort_values(by=["Banka", "Tarih", "Sort"], inplace=True)
+melted_df.sort_values(by=["index", "Sort"], inplace=True)
 V3_Other=melted_df[melted_df["Tutar"]!=0].copy()
 V3_Other.reset_index(drop=True)
 V3_Other['Type'] = V3_Other['Type'].replace({'Banka1': 'Banka', 'Banka2': 'Banka'})
+
+V3_Other.reset_index(inplace=True, drop=True)
 
 IS_Pos=pd.read_excel(r'D:\BulutTahsilat\2024-05\2024-05-ISPOS.xlsx')
 IS_Pos["Tarih"]=IS_Pos["Valör Tarihi"].dt.date
@@ -89,23 +103,25 @@ IS_Pos["Pos"]=-IS_Pos["İşlem Tutarı"]
 IS_Pos["Komisyon"]=IS_Pos["Komisyon Tutarı"]
 IS_Pos["Banka1"]=IS_Pos["Net Tutar"]
 
-melted_is = pd.melt(IS_Pos, id_vars=["Banka", "Firma IBAN", "Tarih"], value_vars=["Banka1", "Pos", "Komisyon"], 
+IS_Pos.reset_index(inplace=True)
+IS_Pos["index"]=IS_Pos["index"]+1000
+
+melted_is = pd.melt(IS_Pos, id_vars=["index", "Banka", "Firma IBAN", "Tarih"], value_vars=["Banka1", "Pos", "Komisyon"], 
                     var_name="Type", value_name="Tutar")
 mapping = {'Banka1': 2, 'Pos': 1, 'Komisyon': 3}
 
 melted_is["Sort"]= melted_is['Type'].map(mapping)
-melted_is.sort_values(by=["Banka", "Tarih", "Sort"], inplace=True)
+melted_is.sort_values(by=["index", "Sort"], inplace=True)
 V3_is=melted_is[melted_is["Tutar"]!=0].copy()
 V3_is.reset_index(drop=True)
 V3_is['Type'] = V3_is['Type'].replace({'Banka1': 'Banka'})
 
-V3_is.reset_index(drop=True)
+V3_is.reset_index(inplace=True, drop=True)
 
 V3= pd.concat([V3_Other, V3_is]).reset_index(drop=True)
 
 melted_Banka = pd.melt(Banka_Conf, id_vars=["Firma IBAN", "BANKA HESAP KODU"], value_vars=["Muhasebe Kodu", "Pos Kodu", "Pos Masraf Kodu"], 
                     var_name="Type", value_name="GLAccCode").dropna(subset=['GLAccCode'])
-
 
 mapping_1 = {'Muhasebe Kodu': "Banka", 'Pos Kodu': 'Pos', "Pos Masraf Kodu": 'Komisyon'}
 
@@ -117,7 +133,7 @@ V3_Format=V3_data[["Tarih"]].copy()
 
 V3_Format.rename(columns={"Tarih": 'DocumentDate'}, inplace=True)
 
-V3_Format["DocumentNumber"]=V3_data.index + 1000
+V3_Format["DocumentNumber"]=V3_data["index"] + 1000
 V3_Format["Description"]=V3_data["Banka"]+" POS İŞLEMLERİ"
 V3_Format["BankCurrAccCode"] = np.where(V3_data["Type"] == "Banka", V3_data["BANKA HESAP KODU"], "")
 V3_Format["BankOpTypeCode"]= 11
